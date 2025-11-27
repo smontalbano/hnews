27589d8308dd7ff040207c9baa92fecd1865ad51
smontalbano <sdm030@gmail.com>
2025-11-27 10:25:58 -0500

    Project setup

---
+++ cmd/web/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type application struct {
+	appName string
+	server  server
+	debug   bool
+	errLog  *log.Logger
+	infoLog *log.Logger
+}
+
+type server struct {
+	host string
+	port string
+	url  string
+}
+
+func main() {
+
+	fmt.Println("Hello world!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/smontalbano/hnews
+
+go 1.22.2
